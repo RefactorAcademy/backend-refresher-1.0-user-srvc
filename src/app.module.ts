bab6b7e85cf6b5ae139393fe318a80ee7193d86a
verne1997 <sourjya@refactor.academy>
2022-12-23 11:26:01 +0530

    added submodules and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './submodules/backend-refresher-1.0-entities/src/entities/user.entity';
 
 
 @Module({
-  imports: [],   // import all my other modules 
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'new-assessment-staging.csdmg3kugxsm.ap-south-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'postgres',
+      password: 'Refactor123456',
+      database: 'backend-socialmedia',
+      entities: [ User ],
+      synchronize: true,
+    }),
+  ],   // import all my other modules 
   controllers: [AppController],    // import all the controller files for this module
   providers: [AppService],     // import the services
 })
